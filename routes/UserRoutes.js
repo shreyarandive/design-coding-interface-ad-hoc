c49cd98190d741373c149d6b2fd1c14a47d4a7c3
Shreya Randive <shreya@pal-nat186-61-225.itap.purdue.edu>
2019-04-25 12:11:14 -0400

    Sequence update at the link /sequence-analysis-temp

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -13,5 +13,6 @@ router.post('/submit-coding', Controller.submitCoding.post);
 router.get('/get-status/:comment_id', Controller.getStatus.get);
 router.get('/visual-analysis/:page_count', Controller.loadMainPage.get);
 router.get('/sequence-analysis', Controller.getAllPostData.get);
+router.get('/sequence-analysis-temp', Controller.getAllPostData.get);
 
 module.exports = router;
